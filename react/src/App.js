d99e9d82a225db370da2c63ef05c91139cd445c8
BokiPanticB <72427445+BokiPanticB@users.noreply.github.com>
2022-04-01 09:34:35 -0700

    init commit

---
+++ react/src/App.js
@@ -0,0 +1,57 @@
+import "./App.css"
+import { useState, useEffect } from "react";
+export default function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:7071/api/toons`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        return response.json();
+      })
+      .then((actualData) => {
+        setData(actualData);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setData(null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Toons Static Web API</h1>
+      <p>Borivoj Pantic, 10224754, bpantic@my.bcit.ca</p>
+      {loading && <div>A moment please...</div>}
+      {error && (
+        <div>{`There is a problem fetching the post data - ${error}`}</div>
+      )}
+   
+      <table class="wp-table">
+           <th>ID</th>
+              <th>Full Name</th>
+              <th>Occupation</th>
+        {data &&
+          data.map(({ id, firstName, lastName, occupation }) => (
+        <><tr>
+           
+            </tr><tr>
+                <td>{id}</td>
+                <td>{firstName} {lastName}</td>
+                <td>{occupation}</td>
+              </tr></>
+                  ))}
+      </table>
+    </div>
+  );
+}
